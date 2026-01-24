6057ae2fb56df4fb3a9de118ec2aae1946a77795
Bivek Gurung <bivek.tamu@gmail.com>
2026-01-24 20:57:01 +1100

    chore:remove unwanted files and configure vite for library mode successful

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
+import dts from 'vite-plugin-dts'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "./tsconfig.lib.json",
+      entryRoot: "src",
+      insertTypesEntry: true,
+      rollupTypes: true,
+    })
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: 'ReactFancySlider',
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime':'jsxRuntime'
+        }
+      }
+    }
+  }
 })
